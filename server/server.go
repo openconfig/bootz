9a6e553d14d88125d8b69a156c3a8e56d19b8e92
Gareth <31602864+gmacf@users.noreply.github.com>
2023-08-08 01:58:52 +0000

    Move server.go to a buildable state.

--- server/server.go
+++ server/server.go
@@ -6,25 +6,34 @@
 package main
 
 import (
-	"github.com/labstack/gommon/log"
+	"flag"
+	"fmt"
+	"net"
+
 	"github.com/openconfig/bootz/proto/bootz"
 	"github.com/openconfig/bootz/server/service"
 	"google.golang.org/grpc"
+
+	log "github.com/golang/glog"
 )
 
 var (
-	port = pflags.StringVar("")
+	port = flag.String("port", "15000", "The port to start the Bootz server on localhost")
 )
 
 func main() {
-	em := &fileentitymanager.Manager{}
-	c, err := service.New(em)
+	flag.Parse()
+	// TODO: Implement an entity manager.
+	c := service.New(nil)
 	s := grpc.NewServer()
-	s.RegisterService(bootz.BootstrapServer, c)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%v", *port))
 	if err != nil {
-		log.Errorf("Failed to start server: %w", err)
+		log.Exitf("Error listening on port: %v", err)
 	}
-	if err := c.Start(); err != nil {
-		log.Fatalf("Server exited: %w", err)
+	bootz.RegisterBootstrapServer(s, c)
+	err = s.Serve(lis)
+	if err != nil {
+		log.Exitf("Error serving grpc: %v", err)
 	}
 }
