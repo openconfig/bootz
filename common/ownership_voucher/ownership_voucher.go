30c9af24be738204f11fc567e33f84f56a4efccf
Michael Zhang <mxz@google.com>
2026-05-19 15:31:57 +1000

    Clean up owner_certificate and ownership_voucher packages. (#319)

--- common/ownership_voucher/ownership_voucher.go
+++ common/ownership_voucher/ownership_voucher.go
@@ -16,19 +16,34 @@
 package ownershipvoucher
 
 import (
+	"crypto"
 	"crypto/x509"
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"time"
 
 	"go.mozilla.org/pkcs7"
-
-	artifacts "github.com/openconfig/bootz/testdata"
 )
 
-// Unmarshal unmarshals the contents of an Ownership Voucher. If a certPool is provided,
-// it is used to verify the contents.
-func Unmarshal(in []byte, certPool *x509.CertPool) (*artifacts.OwnershipVoucher, error) {
+// OwnershipVoucher defines the ownership voucher. See https://www.rfc-editor.org/rfc/rfc8366.html.
+type OwnershipVoucher struct {
+	OV OVInner `json:"ietf-voucher:voucher"`
+}
+
+// OVInner defines the ownership voucher inner structure.
+type OVInner struct {
+	XMLName                    xml.Name `xml:"voucher"`
+	CreatedOn                  string   `json:"created-on" xml:"created-on"`
+	ExpiresOn                  string   `json:"expires-on" xml:"expires-on"`
+	Assertion                  string   `json:"assertion" xml:"assertion"`
+	SerialNumber               string   `json:"serial-number" xml:"serial-number"`
+	PinnedDomainCert           []byte   `json:"pinned-domain-cert" xml:"pinned-domain-cert"`
+	DomainCertRevocationChecks bool     `json:"domain-cert-revocation-checks" xml:"domain-cert-revocation-checks"`
+}
+
+// Unmarshal unmarshals the contents of an ownership voucher. If a certPool is provided, it is used to verify the contents.
+func Unmarshal(in []byte, certPool *x509.CertPool) (*OwnershipVoucher, error) {
 	if len(in) == 0 {
 		return nil, fmt.Errorf("ownership voucher is empty")
 	}
@@ -36,8 +51,8 @@ func Unmarshal(in []byte, certPool *x509.CertPool) (*artifacts.OwnershipVoucher,
 	if err != nil {
 		return nil, fmt.Errorf("unable to parse into pkcs7 format: %v", err)
 	}
-	ov := artifacts.OwnershipVoucher{}
-	jsonErr := json.Unmarshal(p7.Content, &ov)
+	ov := &OwnershipVoucher{}
+	jsonErr := json.Unmarshal(p7.Content, ov)
 	if jsonErr != nil {
 		xmlErr := xml.Unmarshal(p7.Content, &ov.OV)
 		if xmlErr != nil {
@@ -46,8 +61,45 @@ func Unmarshal(in []byte, certPool *x509.CertPool) (*artifacts.OwnershipVoucher,
 	}
 	if certPool != nil {
 		if err = p7.VerifyWithChain(certPool); err != nil {
-			return nil, fmt.Errorf("failed to verify OV: %v", err)
+			return nil, fmt.Errorf("failed to verify ownership voucher: %v", err)
 		}
 	}
-	return &ov, nil
+	return ov, nil
+}
+
+// NewOwnershipVoucher generates an ownership voucher signed by the vendor certificate.
+func NewOwnershipVoucher(encoding string, deviceSerial string, pdc, vendorCert *x509.Certificate, vendorKey crypto.PrivateKey) ([]byte, error) {
+	ov := &OwnershipVoucher{
+		OV: OVInner{
+			CreatedOn:        time.Now().Format(time.RFC3339),
+			ExpiresOn:        time.Now().AddDate(999, 0, 0).Format(time.RFC3339),
+			Assertion:        "verified",
+			SerialNumber:     deviceSerial,
+			PinnedDomainCert: pdc.Raw,
+		},
+	}
+	var ovBytes []byte
+	var err error
+	switch encoding {
+	case "json":
+		if ovBytes, err = json.Marshal(ov); err != nil {
+			return nil, err
+		}
+	case "xml":
+		if ovBytes, err = xml.Marshal(ov.OV); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("unsupported encoding: %v", encoding)
+	}
+	signedMessage, err := pkcs7.NewSignedData(ovBytes)
+	if err != nil {
+		return nil, err
+	}
+	// Override the default SHA1 digest with SHA256.
+	signedMessage.SetDigestAlgorithm(pkcs7.OIDDigestAlgorithmSHA256)
+	if err = signedMessage.AddSigner(vendorCert, vendorKey, pkcs7.SignerInfoConfig{}); err != nil {
+		return nil, err
+	}
+	return signedMessage.Finish()
 }
